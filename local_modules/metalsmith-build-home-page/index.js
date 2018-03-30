81c7d7b023571b3d43c4d58d1a129432a99211c3
werner glinka <werner@bussedesign.com>
2018-03-30 16:01:21 -0500

    completed services section

--- local_modules/metalsmith-build-home-page/index.js
+++ local_modules/metalsmith-build-home-page/index.js
@@ -14,7 +14,8 @@ const commonTags = require('common-tags');
 function plugin() {
     'use strict';
 
-    const getHomePageDataQuery = function (serverURL, page) {
+
+    const getHomePageDataQuery = (serverURL, page) => {
         const query = commonTags.oneLineTrim`?_format=api_json
             &filter[titleFilter][condition][path]=title
             &filter[titleFilter][condition][value]=Orca Home Global
@@ -33,26 +34,28 @@ function plugin() {
                 field_service_byline,
                 field_service_image,
                 field_our_services
-            &include=field_welcome_bg_img,
-                field_about_image,
-                field_testimonials,
-                field_service_image, 
-                field_our_services
             &fields[node--testimonials]=body,
                 field_author,
                 field_title,
                 field_affiliation
             &fields[node--services]=body,
                 field_service_provided,
-                field_service_thumbnail`;
+                field_service_thumbnail,
+            &include=field_welcome_bg_img,
+                field_about_image,
+                field_testimonials,
+                field_service_image,
+                field_our_services,
+                field_our_services.field_service_thumbnail`;
         return serverURL + page + query;
     };
+
 
     // function to get the field value from node and related enties
     // the json api bundles then as attributes and relationships
     // Assumption:
-    // text fields are setup as Text (formatted)
-    const getValue = function (obj, id, field_name, para) {
+    // all text fields are setup as Text (formatted)
+    const getValue = (obj, id, field_name, para) => {
         if (para === 'attr') {
             return obj.data[id].attributes[field_name].value;
         }
@@ -62,59 +65,78 @@ function plugin() {
         return false;
     };
 
-    const getAllTestimonials = function (homePageObj) {
+
+    const getAllTestimonials = (homePageObj) => {
         const testimonials = [];
         const processedTestimonials = [];
 
         // separate testimonials from the rest of the obj
         homePageObj.included.forEach(function (element) {
             if (element.type === "node--testimonials") {
-                testimonials.push(element.attributes);
-            }
-        });
-
-        // retain only the values for each element
-        testimonials.forEach(function(element, index) {
-            let temp = {};
-            for ( let item of Object.keys(element)) {
-                for ( let prop in element[item]) {
-                    if(prop === 'value') {
-                        temp[item] = element[item]['value'];
+                const thisElement = element.attributes;
+                let temp = {};
+                for ( let item of Object.keys(thisElement)) {
+                    for ( let prop in thisElement[item]) {
+                        if(prop === 'value') {
+                            temp[item] = thisElement[item]['value'];
+                        }
                     }
-                } 
+                }
+                testimonials.push(temp);
             }
-            processedTestimonials.push(temp);
         });
-        return processedTestimonials;
+        return testimonials;
     };
+
 
     const getAllServices = (homePageObj) => {
         const services = [];
         const processedServices = [];
+        const availableImages = [];
 
         // separate testimonials from the rest of the obj
         homePageObj.included.forEach(function (element) {
             if (element.type === "node--services") {
-                services.push(element.attributes);
-            }
-        });
+                const thisElement = element.attributes;
 
-        // retain only the values for each element
-        services.forEach(function(element, index) {
-            let temp = {};
-            for ( let item of Object.keys(element)) {
-                for ( let prop in element[item]) {
-                    if(prop === 'value') {
-                        temp[item] = element[item]['value'];
+                // each element object has 'value', 'format' and 'processed' properties
+                // simplify element by only keeping the 'value'
+                // for example:
+                //   "field_service_provided": {
+                //       "value": "Contemplating",
+                //       "format": "basic_html",
+                //       "processed": "Contemplating"
+                //   }
+                //   will become:
+                //  "field_service_provided": "Contemplating"
+                let temp = {};
+                for ( let item of Object.keys(thisElement)) {
+                    for ( let prop in thisElement[item]) {
+                        if(prop === 'value') {
+                            temp[item] = thisElement[item]['value'];
+                        }
                     }
-                } 
+                }
+                // add the relationship image tn
+                temp['image_tn'] = element.relationships.field_service_thumbnail.data.id;
+                services.push(temp);
             }
-            processedServices.push(temp);
+            if (element.type === "file--file") {
+                availableImages.push(element.attributes);
+            }
         });
 
-        console.log(processedServices);
-        return processedServices;
+        // replace the image id with the url in image_tn
+        services.forEach(function (element) {
+            availableImages.forEach(function (thisImage){
+                if (element.image_tn === thisImage.uuid) {
+                    element.image_tn = homePageObj.serverURL + thisImage.uri.url;
+                }
+            });
+        });
+        return services;
     };
+
 
     return function (files, metalsmith, done) {
         setImmediate(done);
@@ -132,13 +154,15 @@ function plugin() {
             }
             // parse json into js object
             const homePageObj = JSON.parse(data);
+            // add serverURL to the page object, we'll need it in some support functions
+            homePageObj.serverURL = serverUrl;
 
             let homePage = {};
 
             // get the fields for the welcome section
             homePage.siteName = getValue(homePageObj, 0, "field_site_name", "attr") || ""; // Text (formatted)
             homePage.welcomeText = getValue(homePageObj, 0, "field_welcome_text", "attr") || ""; // Text (Plain)
-            homePage.bgImageSource = serverUrl + (getValue(homePageObj, 5, "url", "rel") || ""); // Image
+            homePage.bgImageSource = serverUrl + (getValue(homePageObj, 8, "url", "rel") || ""); // Image
             // get the fields for the about section
             homePage.aboutTitle = getValue(homePageObj, 0, "field_about_title", "attr") || ""; // Text (formatted)
             homePage.aboutByline = getValue(homePageObj, 0, "field_about_byline", "attr") || ""; // Text (formatted)
@@ -151,7 +175,7 @@ function plugin() {
             // get the fields for the services section
             homePage.servicesTitle = getValue(homePageObj, 0, "field_service_title", "attr") || ""; // Text (formatted)
             homePage.servicesByline = getValue(homePageObj, 0, "field_service_byline", "attr") || ""; // Text (formatted)
-            homePage.servicesImageSource = serverUrl + (getValue(homePageObj, 1, "url", "rel") || ""); // Image
+            homePage.servicesImageSource = serverUrl + (getValue(homePageObj, 4, "url", "rel") || ""); // Image
             homePage.ourServices = getAllServices(homePageObj); // Array of all services
 
             // write the fields to the home-page data file
